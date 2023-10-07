809f8b1f7d0b3b244a15d5d0c14b45f81f9c68a4
Adam Snow <adamjohnsnow@icloud.com>
2023-10-07 18:10:50 +0100

    add app that reads csv

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+require_relative './lib/toy_robot'
+
+robot = ToyRobot.new
+
+instructions = CSV.read("./files/instructions.csv", headers: false)
+
+instructions.each do |line| 
+  case line[0]
+  when "PLACE"
+    robot.place(line[1].to_i, line[2].to_i, line[3])
+  when "REPORT"
+    p robot.report
+  when "MOVE"
+    robot.move
+  when "LEFT"
+    robot.left
+  when "RIGHT"
+    robot.right
+  end
+end
