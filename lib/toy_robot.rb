473d385a8fad2efd289d337c2c8dd54ffe8ab9a5
Adam Snow <adamjohnsnow@icloud.com>
2023-10-07 14:49:17 +0100

    validates placement

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -4,13 +4,23 @@ DIRECTIONS = [ "NORTH", "EAST", "SOUTH", "WEST" ]
 
 class ToyRobot
   def place x, y, direction
+    return unless valid_placement? x, y, direction
     @x_coordinate = x
     @y_coordinate = y
     @direction = direction
   end
 
   def report
+    return unless @x_coordinate && @y_coordinate && @direction
+    return "#{@x_coordinate}, #{@y_coordinate}, #{@direction}"
+  end
 
-    return @x_coordinate, @y_coordinate, @direction
+  private
+
+  def valid_placement? x, y, direction
+    return false unless x.is_a?(Integer) && x.between?(0, 4)
+    return false unless y.is_a?(Integer) && y.between?(0, 4)
+    return false unless DIRECTIONS.include? direction
+    return true
   end
 end
--- test/test_toy_robot.rb
+++ test/test_toy_robot.rb
@@ -7,7 +7,7 @@ describe "Toy Robot" do
     robot = ToyRobot.new
     position = robot.report
 
-    expect(position).must_equal([nil, nil, nil])
+    refute position
   end
 
   it "gets placed" do
@@ -15,11 +15,34 @@ describe "Toy Robot" do
     robot.place 0, 0, "NORTH"
     position = robot.report
 
-    expect(position).must_equal([0, 0, "NORTH"])
+    expect(position).must_equal("0, 0, NORTH")
 
     robot.place 3, 2, "SOUTH"
     position = robot.report
 
-    expect(position).must_equal([3, 2, "SOUTH"])
+    expect(position).must_equal("3, 2, SOUTH")
+  end
+
+  it "validates placement" do
+    robot = ToyRobot.new
+    robot.place -1, 0, "NORTH"
+    position = robot.report
+
+    refute position
+
+    robot.place "X", 0, "NORTH"
+    position = robot.report
+
+    refute position
+
+    robot.place 1, 5, "NORTH"
+    position = robot.report
+
+    refute position
+
+    robot.place 1, 1, "TEST"
+    position = robot.report
+
+    refute position
   end
 end
